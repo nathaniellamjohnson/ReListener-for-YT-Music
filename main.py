caaf3acd301a19e925d834ad989c22b5fb1e8e4b
Nathaniel L Johnson <nathanieljohnson009@gmail.com>
2022-07-15 10:39:09 -0700

    Added full functionality, gets listen again page, creates weekly playlist, and updates forever playlist - also pulls headers from a headers_raw.txt file

--- main.py
+++ main.py
@@ -1,37 +1,85 @@
 from ytmusicapi import YTMusic
 from datetime import date
 
-YTMusic.setup(filepath='headers_auth.json', headers_raw= """ <INSERT_HEADERS_HERE> """)
+# requires a headers_raw.txt with raw header data to get 
+with open('headers_raw.txt') as headers_raw_file:
+    headers_raw_text = headers_raw_file.read()
+    print(headers_raw_text)
+
+YTMusic.setup(filepath='headers_auth.json', headers_raw= headers_raw_text)
 ytmusic = YTMusic('headers_auth.json')
 
-if __name__ == '__main__':
 
-    # Get your Listen Again Songs
-    home = ytmusic.get_home(limit=10)
+def get_section_of_home(home_list, sec_name):
     i = 0
-    for items in home: # Finds the Listen Again page
-        if items['title'] == 'Listen again':
+    for items in home_list:  # Finds the Listen Again page
+        if items['title'] == sec_name:
             break
         i = i + 1
-    videoIdList = []
-    for songs in home[i]['contents']:
+    return i
+
+
+def get_all_songs_in_section(home_list, index_of_section):
+    list_of_songs_to_append_to = []
+    for songs in home_list[index_of_section]['contents']:
         a = songs.get('videoId')
         if a is not None:
-            videoIdList.append(a)
+            list_of_songs_to_append_to.append(a)
+    return list_of_songs_to_append_to
+
+
+def clean_today_date():
+    # returns string for date in format of Month(text) Day, Year
     today = date.today()
-    today = today.strftime("%B %d, %Y")  # Date in Month (text) Day, Year
-    title = "Weekly ReListener - " + today
-    newWeeklyPlaylistID = ytmusic.create_playlist(title=title, description="Created by ReListener at https://github.com/nathaniellamjohnson/ReListener-for-YT-Music on " + today, video_ids=videoIdList)
-    foreverPlaylistID = None
-    userPlaylists = ytmusic.get_library_playlists(limit=1000)
-    for playlists in userPlaylists:
-        if playlists['title'] == "Forever ReListener":
-            foreverPlaylistID = playlists['playlistId']
-    if foreverPlaylistID is not None:
-        ytmusic.edit_playlist(playlistId=foreverPlaylistID,
-                              description="Created by ReListener at https://github.com/nathaniellamjohnson/ReListener-for-YT-Music, last edited on " + today,
-                              addPlaylistId=newWeeklyPlaylistID)
-    else:
-        ytmusic.create_playlist(title="Forever ReListener",
-                                description="Created by ReListener at https://github.com/nathaniellamjohnson/ReListener-for-YT-Music, last edited on " + today,
-                                source_playlist=newWeeklyPlaylistID)
+    today = today.strftime("%B %d, %Y")
+    return today
+
+
+def update_forever_playlist(video_id_list):
+    # Get the forever playlist id / if doesn't exist, make one
+    foreverplaylistid = None
+    userplaylists = ytmusic.get_library_playlists(limit=1000)
+
+    for playlist in userplaylists:
+        if playlist['title'] == "Forever ReListener":
+            foreverplaylistid = playlist['playlistId']
+
+    if foreverplaylistid is None:
+        # No forever playlist found, make one & put all the video ids from the weekly in
+        foreverplaylistid = ytmusic.create_playlist(title="Forever ReListener",
+                                                    description="Created by ReListener at "
+                                                                "https://github.com/nathaniellamjohnson/ReListener"
+                                                                "-for-YT-Music, last edited on " + clean_today_date(),
+                                                    video_ids=videoIdList)
+        return
+
+    # If forever playlist is present
+    # take song ids, check against song ids of forever playlist, remove duplicates
+    foreverplaylist = ytmusic.get_playlist(playlistId=foreverplaylistid, limit=10000)
+    foreverplaylistsongids = []
+    for obj in foreverplaylist['tracks']:
+        foreverplaylistsongids.append(obj['videoId'])
+    # Loop through video id list
+    valid_ids = []
+    for song in video_id_list:
+        if song not in foreverplaylistsongids:
+            valid_ids.append(song)
+    ytmusic.add_playlist_items(playlistId=foreverplaylistid, videoIds=valid_ids)
+    ytmusic.edit_playlist(description="Created by ReListener at https://github.com/nathaniellamjohnson/ReListener-for"
+                                      "-YT-Music, last edited on " + clean_today_date())
+    return
+
+
+if __name__ == '__main__':
+    home = ytmusic.get_home(limit=10)
+    indexOfListenAgain = get_section_of_home(home, 'Listen again')
+
+    videoIdList = get_all_songs_in_section(home, indexOfListenAgain)
+
+    newWeeklyPlaylistID = ytmusic.create_playlist(title="Weekly ReListener - " + clean_today_date(),
+                                                  description="Created by ReListener at "
+                                                              "https://github.com/nathaniellamjohnson/ReListener-for"
+                                                              "-YT-Music on " + clean_today_date(),
+                                                  video_ids=videoIdList)
+
+    update_forever_playlist(videoIdList)
